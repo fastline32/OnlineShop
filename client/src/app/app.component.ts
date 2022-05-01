ef20f7bea843419736d072dd2ac70f8fd1304baa
Svetoslav <speednet@mail.bg>
2022-05-01 14:24:23 +0300

    Setting Up Angular relation with NetCore Api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'OnlineShop';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/onlineshop/product?pageSize=20').subscribe((response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
