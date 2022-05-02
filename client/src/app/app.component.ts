a571cf2a6efdb2b9ad1b3e75dc3bc4072f75e347
Svetoslav <speednet@mail.bg>
2022-05-02 12:12:10 +0300

    Search and Filter Funcionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +7,12 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit{
   title = 'OnlineShop';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {
+  constructor() {
 
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/onlineshop/product?pageSize=20').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+    
   }
 }
